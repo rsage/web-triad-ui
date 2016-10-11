22ebf7398094fbb3dd5aa644da8c3b2c84b1e035
vromanenko <vromanenko@acr.org>
2016-10-11 08:16:16 -0400

    add view for testing

---
+++ web-triad-ui/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_triad_ui.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        public RedirectToRouteResult Index()
+        {
+            ViewBag.Title = "ReviewControl";
+            return RedirectToAction("ReviewControl");
+        }
+
+        public ActionResult ReviewControl()
+        {
+            return View();
+        }
+    }
+}
